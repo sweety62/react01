4c30e8f986cc005454453ca57f520dd493c0c82c
sweety62 <myselfsweety1@gmail.com>
2026-02-17 23:12:55 +0530

    feat: add functions for fruit, vegetable, addition, and operations in App component

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,49 @@ function App(){
     const userName = "sweety";
     let X =10;
     let Y =20;
-    return (
+    function Fruit(){
+        return "Mango";
+    }
+    function Vegetable(){
+        return "Carrot";
+    }
+    function Add(X,Y){
+        return X + Y;
+    }
+
+    function operation(a,b,op){
+        if(op === "add"){
+            return Add(a,b);
+        }else if(op === "multiply"){
+            return a * b;
+        }
+        else{
+            return "Invalid operation";
+        }
+    }
+
+    /*return (
         <div>
             <h1>{userName}</h1>
             <h1>{X + Y}</h1>
             <h1>{X * Y}</h1>
         </div>
-    )
+    )*/
+
+        return(
+            <div>
+                <h1>JSX with curly braces</h1>
+                <h1>{userName}</h1>
+                <h1>{X + Y}</h1>
+                <h1>{X * Y}</h1>
+                <h1>{Fruit()}</h1>
+                <h1>{Vegetable()}</h1>
+                <h1>{Add(X,Y)}</h1>
+                <h1>{operation(X,Y,"add")}</h1>
+                <h1>{operation(X,Y,"multiply")}</h1>
+                <h1>{operation(X,Y,"divide")}</h1>
+            </div>
+        )
 }
 export default App;
 
