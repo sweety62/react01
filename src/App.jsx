9349afe2b292d2530dfd2f2b18dd6c7a3bc618b9
sweety62 <myselfsweety1@gmail.com>
2026-02-17 20:58:28 +0530

    feat: initialize React project with Vite, add basic components and styles
    - Created package.json with dependencies for React, Vite, and ESLint.
    - Added Vite logo SVG to public directory.
    - Implemented App component with Login, Profile, and Setting components.
    - Created UserComponent for user-related functionalities.
    - Added styles for the application in App.css and index.css.
    - Configured main entry point in main.jsx.
    - Set up Vite configuration in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import Login ,{Profile,Setting,Userkey}from "./UserComponent";
+function App(){
+    return (
+        <div>
+            <h1>Exporting  and Importing Component</h1>
+            <Login></Login>
+            <Profile></Profile>
+            <Setting></Setting>
+            <h1>{Userkey}</h1>
+
+        </div>
+
+    )
+}
+
+
+
+ 
+
+
+
+export default App;
+
+
+
+
+
+
+
+
+
